cbcb26bcb171c60acb0516a2958d94416c67478d
Martin Nicola <lehitutoring@gmail.com>
2023-05-04 12:05:59 -0600

    styling: changed certain colors in landing component

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className="Landing">
       <div className="blurb">
         <h1>Hi, I'm Martin</h1>
-        <p>I'm a Software Developer <br/> in the clouds!</p>
+        <p>I'm a Software Developer who loves <br/> databases, backend, and cloud computing!</p>
         <a href="resume/marty_resume_2023.pdf" download>
           <FontAwesomeIcon icon={faFileAlt} size="3x"/>
           <h5>Download Resume</h5>
